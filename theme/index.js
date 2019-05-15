3d63226cb1396c00123c9cb5ec671e73e3a88fc7
Paul C Pederson <paul.c.pederson@gmail.com>
2019-05-14 17:57:25 -0700

    working documentation

---
+++ theme/index.js
@@ -0,0 +1,41 @@
+var themeleon = require('themeleon')().use('swig')
+var theme = themeleon(__dirname, function (t) {
+  t.swig('index.html.swig', 'index.html')
+})
+
+function parseEntry (d) {
+  return {
+    description: d.description,
+    name: d.context.name,
+    code: d.context.code,
+    parameters: d.parameter,
+    type: d.context.type,
+    example: d.example
+  }
+}
+
+module.exports = function (dest, ctx) {
+  ctx.groups = [
+    {
+      title: 'Spacing', 
+      mixins: ctx.data.filter(d => d.group[0] === 'spacing').map(parseEntry)
+    },
+    {
+      title: 'Type', 
+      mixins: ctx.data.filter(d => d.group[0] === 'type').map(parseEntry)
+    },
+    {
+      title: 'Utilities',
+      mixins: ctx.data.filter(d => d.group[0] === 'utilities').map(parseEntry)
+    },
+    {
+      title: 'Animation',
+      mixins: ctx.data.filter(d => d.group[0] === 'animation').map(parseEntry)
+    },
+    {
+      title: 'Responsive',
+      mixins: ctx.data.filter(d => d.group[0] === 'responsive').map(parseEntry)
+    }
+  ]
+  return theme.apply(this, arguments)
+}
